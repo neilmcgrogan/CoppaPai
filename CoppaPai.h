e39d7ee2b3b07b100ec45659fb433fef3aa5ab3c
Neil <mcgrogan.neil@gmail.com>
2023-09-25 21:04:10 -0400

    initial test function

---
+++ CoppaPai.h
@@ -0,0 +1,18 @@
+//
+//  CoppaPai.h
+//  CoppaPai
+//
+//  Created by Neil McGrogan on 9/25/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CoppaPai.
+FOUNDATION_EXPORT double CoppaPaiVersionNumber;
+
+//! Project version string for CoppaPai.
+FOUNDATION_EXPORT const unsigned char CoppaPaiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoppaPai/PublicHeader.h>
+
+
---
+++ CoppaPai/Example.swift
@@ -0,0 +1,12 @@
+//
+//  Example.swift
+//  CoppaPai
+//
+//  Created by Neil McGrogan on 9/25/23.
+//
+
+import Foundation
+
+func test() {
+    print("Works...")
+}
