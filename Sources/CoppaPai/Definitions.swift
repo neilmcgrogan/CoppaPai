960df8f6b5692c936b39a1c5570ae4f80ca36f34
Neil <mcgrogan.neil@gmail.com>
2023-10-09 21:29:21 -0400

    add review functionality

---
+++ Sources/CoppaPai/Definitions.swift
@@ -0,0 +1,11 @@
+//
+//  Definitions.swift
+//  CoppaPai
+//
+//  Created by Neil McGrogan on 10/9/23.
+//
+
+import SwiftUI
+
+public let screen = UIScreen.main.bounds
+public let defaults = UserDefaults.standard
---
+++ Sources/CoppaPai/UserData.swift
@@ -0,0 +1,20 @@
+//
+//  UserData.swift
+//  CoppaPai
+//
+//  Created by Neil McGrogan on 10/9/23.
+//
+
+import SwiftUI
+
+@available(iOS 13.0, *)
+class UserData: ObservableObject {
+    
+    @Published var gamesPlayed: Int {
+        didSet { defaults.set(gamesPlayed, forKey: "games_played") }
+    }
+    
+    init() {
+        self.gamesPlayed = defaults.object(forKey: "games_played") as? Int ?? 0
+    }
+}
