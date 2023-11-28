683c2aba600fcf9eabdff531396a9a6ebadac4e9
Neil <mcgrogan.neil@gmail.com>
2023-11-28 16:18:45 -0500

    add device detection

--- Sources/CoppaPai/DeviceDetection.swift
+++ Sources/CoppaPai/DeviceDetection.swift
@@ -14,3 +14,28 @@ public func isIPad() -> Bool {
         return false
     }
 }
+
+@available(iOS 13.0, *)
+public struct DeviceRotationViewModifier: ViewModifier {
+    let action: (UIDeviceOrientation) -> Void
+
+    public init(action: @escaping (UIDeviceOrientation) -> Void) {
+        self.action = action
+    }
+    
+    public func body(content: Content) -> some View {
+        content
+            .onAppear()
+            .onReceive(NotificationCenter.default.publisher(for: UIDevice.orientationDidChangeNotification)) { _ in
+                action(UIDevice.current.orientation)
+            }
+    }
+}
+
+@available(iOS 13.0, *)
+// A View wrapper to make the modifier easier to use
+extension View {
+    public func onRotate(perform action: @escaping (UIDeviceOrientation) -> Void) -> some View {
+        self.modifier(DeviceRotationViewModifier(action: action))
+    }
+}
