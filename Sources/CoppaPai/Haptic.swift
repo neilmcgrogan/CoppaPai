a9e821aa5dfde750d3479f2be93c420b13f26a99
Neil <mcgrogan.neil@gmail.com>
2023-12-13 20:09:21 -0500

    fix haptics

--- Sources/CoppaPai/Haptic.swift
+++ Sources/CoppaPai/Haptic.swift
@@ -8,14 +8,13 @@
 import SwiftUI
 
 @available(iOS 13.0, *)
-public struct Response {
-    public func softHaptic() {
-        let impactMed = UIImpactFeedbackGenerator(style: .soft)
-        impactMed.impactOccurred()
-    }
-    
-    public func heavyHaptic() {
-        let impactMed = UIImpactFeedbackGenerator(style: .medium)
-        impactMed.impactOccurred()
-    }
+public func softHaptic() {
+    let impactMed = UIImpactFeedbackGenerator(style: .soft)
+    impactMed.impactOccurred()
+}
+
+@available(iOS 13.0, *)
+public func heavyHaptic() {
+    let impactMed = UIImpactFeedbackGenerator(style: .medium)
+    impactMed.impactOccurred()
 }
