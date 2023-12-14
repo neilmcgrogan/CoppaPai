c570ea01b51e4783439667e636de6aa3670d12fa
Neil <mcgrogan.neil@gmail.com>
2023-12-13 22:42:34 -0500

    adjust color

--- Sources/CoppaPai/Styles/Extension+Color.swift
+++ Sources/CoppaPai/Styles/Extension+Color.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 @available(iOS 13.0, *)
 extension Color {
-    public static var primary_pink: Color = Color(red: 253/255, green: 158/255, blue: 155/255)
+    public static var primary_pink: Color = Color(red: 247/255, green: 159/255, blue: 157/255)
     public static var dark_gray: Color = Color(red: 140/255, green: 140/255, blue: 140/255)
     
 }
