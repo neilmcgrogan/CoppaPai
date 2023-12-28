248636d472a75074704646e0579cad0adc426257
Neil <mcgrogan.neil@gmail.com>
2023-12-28 07:01:04 -0500

    Add confetti animation

---
+++ Sources/CoppaPai/Views/AskForReview.swift
@@ -0,0 +1,97 @@
+//
+//  AskForReview.swift
+//  CoppaPai
+//
+//  Created by Neil McGrogan on 12/28/23.
+//
+
+import SwiftUI
+
+@available(iOS 15.0, *)
+public struct AskForReview: View {
+    public let title: String
+    @Binding public var askFeedback: Bool
+    var storeReview = StoreReview()
+    
+    public init(title: String, askFeedback: Binding<Bool>) {
+        self.title = title
+        self._askFeedback = askFeedback
+    }
+    
+    public var body: some View {
+        ZStack {
+            Color.gray.opacity(0.01)
+            
+            VStack(spacing: 20) {
+                Text(title)
+                    .bold()
+                    .font(.title)
+                HStack {
+                    Image("star")
+                        .resizable()
+                        .frame(width: 20, height: 20)
+                    Image("star")
+                        .resizable()
+                        .frame(width: 22.5, height: 22.5)
+                        .offset(y: -2.5)
+                    Image("star")
+                        .resizable()
+                        .frame(width: 25, height: 25)
+                        .offset(y: -5)
+                    Image("star")
+                        .resizable()
+                        .frame(width: 22.5, height: 22.5)
+                        .offset(y: -2.5)
+                    Image("star")
+                        .resizable()
+                        .frame(width: 20, height: 20)
+                }
+                
+                Text("Help us with a review")
+                    .bold()
+                    .font(.body)
+                Spacer()
+                HStack {
+                    Button(action: {
+                        askFeedback.toggle()
+                    }) {
+                        Text("No")
+                            .padding(.vertical, 10)
+                            .padding(.horizontal, 30)
+                            .menuViewStyle()
+                            .padding(1)
+                            .background(Color.gray.cornerRadius(30))
+                            .frame(maxWidth: .infinity)
+                    }.buttonStyle(ScaleButton())
+                    
+                    Button(action: {
+                        storeReview.requestReview()
+                        askFeedback.toggle()
+                    }) {
+                        Text("Yes")
+                            .fontWeight(.medium)
+                            .padding(.vertical, 10)
+                            .padding(.horizontal, 30)
+                            .padding(1)
+                            .background(Color.green.cornerRadius(30))
+                            .springEffect(animationTrigger: .constant(true))
+                            .frame(maxWidth: .infinity)
+                    }.buttonStyle(ScaleButton())
+                }
+                .font(.title2)
+            }
+            .padding()
+            .frame(width: 285, height: 285)
+            .menuViewStyle()
+        }
+        .transition(.opacity)
+    }
+}
+
+@available(iOS 15.0, *)
+#Preview {
+    AskForReview(title: "Loving _ game?", askFeedback: .constant(false))
+        .background(Color.menuTitle)
+        .previewLayout(PreviewLayout.sizeThatFits)
+        .colorScheme(.dark)
+}
---
+++ Sources/CoppaPai/Views/HelpView.swift
@@ -0,0 +1,63 @@
+//
+//  HelpView.swift
+//  CoppaPai
+//
+//  Created by Neil McGrogan on 12/28/23.
+//
+
+import SwiftUI
+
+@available(iOS 16.0, *)
+public struct HelpView: View {
+    @Binding var showHelp: Bool
+    let texts: [(String, String)]
+    
+    public init(showHelp: Binding<Bool>, texts: [(String, String)]) {
+        self._showHelp = showHelp
+        self.texts = texts
+    }
+    
+    public var body: some View {
+        VStack(alignment: .leading) {
+            Button(action: {
+                withAnimation() {
+                    self.showHelp = false
+                }
+            }) {
+                HStack {
+                    Text("How to Play")
+                        .bold()
+                    Spacer()
+                    Image(systemName: "xmark.circle")
+                        .foregroundColor(.gray)
+                        .fontWeight(.semibold)
+                }
+                .font(.title)
+            }
+            
+            ScrollView {
+                VStack(alignment: .leading) {
+                    ForEach(texts.indices, id: \.self) { index in
+                        let text = texts[index]
+                        Group {
+                            Text(text.0).bold()
+                            Text(LocalizedStringKey(text.1))
+                        }
+                    }
+                }
+                .padding()
+                .background(Color.gray.opacity(0.2).cornerRadius(10))
+                .multilineTextAlignment(.leading)
+            }
+        }
+        .fontWeight(.light)
+        .padding()
+        .menuViewStyle()
+        .padding()
+    }
+}
+
+@available(iOS 16.0, *)
+#Preview {
+    HelpView(showHelp: .constant(false), texts: [("Guess two words in 6 tries.", "- Each guess must be a valid 5-letter word.\nThe color of the tiles will change to show how close your guess was to the word."), ("Examples.", "- Green is in the word and in the correct spot.\n- Yellow is in the word but in the wrong spot.\n- Gray is not in the word in any spot.")])
+}
