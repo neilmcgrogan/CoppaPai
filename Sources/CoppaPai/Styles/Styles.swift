676e214227ae04eb8c254af3910566abc6f4437f
Neil <mcgrogan.neil@gmail.com>
2024-01-06 21:43:04 -0500

    edit outline for the inclusion of dark mode

--- Sources/CoppaPai/Styles/Styles.swift
+++ Sources/CoppaPai/Styles/Styles.swift
@@ -10,18 +10,24 @@ import UIKit
 
 @available(iOS 16.0, *)
 extension View {
-    public func outlined() -> some View {
-        self
-            .background(LinearGradient(colors: [Color.white.opacity(0.7), Color.white.opacity(0.35)], startPoint: .bottom, endPoint: .top))
-            .overlay(
-                RoundedRectangle(cornerRadius: 16)
-                .stroke(LinearGradient(colors: [Color.white.opacity(0.3), Color.white.opacity(0.025)], startPoint: .bottom, endPoint: .top), lineWidth: 4)
-            )
-            .clipShape(RoundedRectangle(cornerRadius: 16))
-            .padding(.horizontal, 16)
-            .padding(.vertical, 16)
-            .shadow(color: .black.opacity(0.1), radius: 80, x: 0, y: 100)
-            .shadow(color: .black.opacity(0.14), radius: 10, x: 0, y: 12.5)
+    public func outlined(dark: Bool) -> some View {
+        if dark {
+            self
+                .background(LinearGradient(colors: [Color.gray.opacity(0.80), Color.gray.opacity(0.35)], startPoint: .bottom, endPoint: .top))
+                .clipShape(RoundedRectangle(cornerRadius: 16))
+                .padding(.horizontal, 16)
+                .padding(.vertical, 16)
+                .shadow(color: .white.opacity(0.1), radius: 80, x: 0, y: 100)
+                .shadow(color: .white.opacity(0.14), radius: 10, x: 0, y: 12.5)
+        } else {
+            self
+                .background(LinearGradient(colors: [Color.white.opacity(0.7), Color.white.opacity(0.35)], startPoint: .bottom, endPoint: .top))
+                .clipShape(RoundedRectangle(cornerRadius: 16))
+                .padding(.horizontal, 16)
+                .padding(.vertical, 16)
+                .shadow(color: .black.opacity(0.1), radius: 80, x: 0, y: 100)
+                .shadow(color: .black.opacity(0.14), radius: 10, x: 0, y: 12.5)
+        }
     }
     
     public func sectionTitle() -> some View {
